3aa89a07423c2e587890a5e6f533bfaafb28214e
Phil Johnson <phytoporg@gmail.com>
2015-09-01 23:52:07 -0700

    HTC.SettingsAccess initial revision

---
+++ HTC.SettingsAccess/SettingsAccessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Linq;
+
+namespace HTC
+{
+    namespace SettingsAccess
+    {
+        public class SettingsAccessor
+        {
+            private readonly KeyValueConfigurationCollection _keyValueConfigCollection;
+            public SettingsAccessor(Configuration config)
+            {
+                _keyValueConfigCollection = config.AppSettings.Settings;
+            }
+
+            public bool GetConfigValue(string key, out string value)
+            {
+                value = string.Empty;
+                if (!_keyValueConfigCollection.AllKeys.Contains(key))
+                {
+                    return false;
+                }
+                else
+                {
+                    value = _keyValueConfigCollection[key].Value;
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ HTC.SettingsAccess/SettingsMutator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTC
+{
+    namespace SettingsAccess
+    {
+        public class SettingsMutator
+        {
+            private KeyValueConfigurationCollection _keyValueConfigCollection;
+            private Configuration _config;
+            public SettingsMutator(Configuration config)
+            {
+                _config = config;
+                _keyValueConfigCollection = config.AppSettings.Settings;
+            }
+
+            public void SetConfigValue(string value, string key)
+            {
+                _config.AppSettings.Settings.Add(
+                    new KeyValueConfigurationElement(value, key)
+                    );
+            }
+
+            public bool CommitChanges()
+            {
+                if (_config.GetSection("appSettings").SectionInformation.IsLocked)
+                {
+                    return false;
+                }
+                else
+                {
+                    _config.Save();
+                    return true;
+                }
+            }
+        }
+    }
+}
